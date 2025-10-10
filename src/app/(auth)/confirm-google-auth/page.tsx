0be72c0de26201a8f5eaf5667f12002ddc3b2f93
Andrii Kostenko <Kostenkoandrey14@gmail.com>
2025-10-10 11:59:35 -0300

    last

--- src/app/(auth)/confirm-google-auth/page.tsx
+++ src/app/(auth)/confirm-google-auth/page.tsx
@@ -1,6 +1,34 @@
 'use client';
 
+import React, { useEffect, useRef } from 'react';
+import { useRouter } from 'next/navigation';
+import { confirmGoogleAuthThunk } from '@/redux/auth/operations';
+import { useAppDispatch } from '@/hooks/useReduxHooks';
+
 const Page = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const hasRun = useRef(false);
+
+  useEffect(() => {
+    if (hasRun.current) return;
+    hasRun.current = true;
+
+    const code = new URLSearchParams(window.location.search).get('code');
+    if (!code) return;
+
+    (() => {
+      dispatch(confirmGoogleAuthThunk({ code }))
+        .unwrap()
+        .then(() => {
+          router.push('/dashboard');
+        })
+        .catch((err) => {
+          router.push('/login');
+        });
+    })();
+  }, []);
+
   return (
     <div className='flex flex-col items-center justify-center h-screen text-white text-3xl'>
       Авторизация через Google...
