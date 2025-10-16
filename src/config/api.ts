09163f8036b51cd183832be54dd1b5aa5f1e0343
Andrii Kostenko <Kostenkoandrey14@gmail.com>
2025-10-16 14:06:41 -0300

    add backen

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,3 +2,8 @@ export const config = {
   apiUrl: 'https://realtime-api-backend.onrender.com',
   wsUrl: 'wss://realtime-api-backend.onrender.com',
 };
+
+// export const config = {
+//   apiUrl: 'https://realtime-api-backend.onrender.com',
+//   wsUrl: 'wss://realtime-api-backend.onrender.com',
+// };
