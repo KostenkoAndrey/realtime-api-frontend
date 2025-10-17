d3a0674471b11c4d1a3e96f42b684654a812de70
Andrii Kostenko <Kostenkoandrey14@gmail.com>
2025-10-17 13:45:31 -0300

    add url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,9 +1,9 @@
-// export const config = {
-//   apiUrl: 'https://realtime-api-backend.onrender.com',
-//   wsUrl: 'wss://realtime-api-backend.onrender.com',
-// };
-
 export const config = {
-  apiUrl: 'http://localhost:3000',
-  wsUrl: 'ws://localhost:3000',
+  apiUrl: 'https://realtime-api-backend.onrender.com',
+  wsUrl: 'wss://realtime-api-backend.onrender.com',
 };
+
+// export const config = {
+//   apiUrl: 'http://localhost:3000',
+//   wsUrl: 'ws://localhost:3000',
+// };
